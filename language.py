58a1771fe7e3e7485c1e108721d517b7b4beff1c
Douglas Bagnall <douglas@halo.gen.nz>
2015-04-11 23:35:38 +1200

    corpus-utils creates corpus taming mappings
    The corpora contain a number of unhelpful rare and stray characters, many
    of which can be mapped together to reduce the alphabet size.
    
    This will create a python file containing the mapping in a dictionary.
    
        ./corpus-utils -m corpus/whatever -d --decompose-caps > \
             mappings/whatever.py
    
    I wasn't initially going to do it this way, rather create the mapping at
    run time, but printing it out is useful to see what's going on.

--- language.py
+++ language.py
@@ -11,8 +11,10 @@ TEST_FN_PATTERN = r'^unknown\.txt$'
 training_fn_search = re.compile(TRAINING_FN_PATTERN).search
 test_fn_search = re.compile(TRAINING_FN_PATTERN).search
 
+
 def always(x):
     return True
+
 
 def load_texts(srcdir, class_accept=always, file_accept=training_fn_search):
     texts = {}
@@ -37,11 +39,24 @@ def concat_corpus(srcdir, exclude_test=False):
     all_texts = []
     for v in texts.values():
         all_texts.extend(v)
-    return ''.join(all_texts)
+    return '\n'.join(all_texts)
 
-def count_chars(text):
-    c = Counter(text.decode('utf-8'))
+
+def count_chars(text, decompose=False):
+    text = text.decode('utf-8')
+    if decompose:
+        text = unicodedata.normalize('NFKD', text)
+    else:
+        text = unicodedata.normalize('NFKC', text)
+    c = Counter(text)
     return c.most_common()
 
 def fix_en_chars():
     pass
+
+dispensible_chars = set('\x0b\x0c\r'.decode('utf8') + u'\ufeff\xad\x85' +
+                        u'\u2028')
+
+single_quotes = set("'‘’‘‘".decode('utf8') + u'\u2018')
+
+double_quotes = set('‟"„“”'.decode('utf8'))
