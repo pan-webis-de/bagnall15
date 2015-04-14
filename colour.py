855cc001323af8a76e0e62480116aff50658a7e8
Douglas Bagnall <douglas@halo.gen.nz>
2015-04-14 18:24:05 +1200

    import terminal colours

---
+++ colour.py
@@ -0,0 +1,18 @@
+
+C_NORMAL    = "\033[00m"
+DARK_RED    = "\033[00;31m"
+RED         = "\033[01;31m"
+DARK_GREEN  = "\033[00;32m"
+GREEN       = "\033[01;32m"
+YELLOW      = "\033[01;33m"
+DARK_YELLOW = "\033[00;33m"
+DARK_BLUE   = "\033[00;34m"
+BLUE        = "\033[01;34m"
+PURPLE      = "\033[00;35m"
+MAGENTA     = "\033[01;35m"
+DARK_CYAN   = "\033[00;36m"
+CYAN        = "\033[01;36m"
+GREY        = "\033[00;37m"
+WHITE       = "\033[01;37m"
+
+REV_RED     = "\033[01;41m"
