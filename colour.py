c7ed719783ab15c11b911e9cf177d677be14b936
Douglas Bagnall <douglas@halo.gen.nz>
2015-04-26 13:48:16 +1200

    calc-scores looks at the best 10 for each range

--- colour.py
+++ colour.py
@@ -16,3 +16,19 @@ GREY        = "\033[00;37m"
 WHITE       = "\033[01;37m"
 
 REV_RED     = "\033[01;41m"
+
+_FOREGROUND = "\033[38;5;%sm"
+
+
+def generate_spectrum(starts, step=6, n=6):
+    spectrum = []
+    for start in starts:
+        end = start + step * n
+        spectrum.extend(_FOREGROUND % x for x in range(start, end, step))
+    return spectrum
+
+spectra = {k: generate_spectrum(x) for k, x
+           in (('warm', (160, 196)),
+                ('darkwarm', (88, 124)),
+                ('bluegreen', (17, 53)),
+           )}
