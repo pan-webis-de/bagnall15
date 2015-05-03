64c5477f7f393def1d42ff2c6f280fbac96b331a
Douglas Bagnall <douglas@halo.gen.nz>
2015-05-03 15:12:10 +1200

    FIX --raw-answers-trace, and get into make-answers

--- make-answers.sh
+++ make-answers.sh
@@ -13,5 +13,6 @@ fi
 for n in $languages; do
     echo $n
     mkdir -p $DEST/$n
-    time ./pan-test -i corpus/*$n* -o $DEST/$n --raw-answers  2>>$DEST/stderr-$n.log
+    time ./pan-test -i corpus/*$n* -o $DEST/$n --raw-answers --raw-answers-trace \
+       2>>$DEST/stderr-$n.log
  done
